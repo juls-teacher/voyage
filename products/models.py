0ff8aaf4b816a46daec2a135509fe65240b8cf75
juls-teacher <juliayurchyk@gmail.com>
2023-06-28 00:11:15 +0300

    add models customer, adress, order, orderitem

--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.conf import settings
+
 from decimal import Decimal
+from django.contrib.auth.models import User
 
 
 class Product(models.Model):
@@ -13,3 +14,40 @@ class Product(models.Model):
 
     def __str__(self):
         return f"Product : {self.title} - {self.price}"
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=250, null= True)
+    email = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=100, null=True)
+
+    def str(self):
+        return str(self.id)
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+class Address(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length=200, null=False)
+    state = models.CharField(max_length=200, null=False)
+    zipcode = models.CharField(max_length=200, null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def str(self):
+        return self.address
+
+
