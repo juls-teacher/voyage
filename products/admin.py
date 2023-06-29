dfae6c446e0216cd62b9f8a485b59d83eb8ec7c7
juls-teacher <juliayurchyk@gmail.com>
2023-06-29 22:44:34 +0300

    update admin file

--- products/admin.py
+++ products/admin.py
@@ -1,13 +1,45 @@
 from django.contrib import admin
-from products.models import Product
+from products.models import Product, Customer, Order, OrderItem, Address
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("title", "price", "description", "created_at")
-    fields = ["title", "image", "price", "description", "created_at"]
-    readonly_fields = ("created_at",)
+    list_display = ("title", "price", "description",)
+    fields = ["title", "image", "price", "description",]
+    readonly_fields = ()
     search_fields = ("title", "description")
 
     def save_form(self, request, form, change):
         return super().save_form(request, form, change)
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ("user", "name", "email",)
+    fields = ["user", "name", "email",]
+    readonly_fields = ("email",)
+    search_fields = ("user", "name",)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("customer", "date_ordered", "complete", "transaction_id",)
+    fields = ["customer", "date_ordered", "complete", "transaction_id", ]
+    readonly_fields = ("date_ordered",)
+    search_fields = ("customer",)
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ("product", "order", "quantity", "date_added",)
+    fields = ["product", "order", "quantity", "date_added", ]
+    readonly_fields = ("date_added",)
+    search_fields = ("product",)
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ("customer", "order", "address", "city", "state", "zipcode", "date_added",)
+    fields = ["customer", "order", "address", "city", "state", "zipcode", "date_added", ]
+    readonly_fields = ("date_added",)
+    search_fields = ("city",)
--- products/models.py
+++ products/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 from decimal import Decimal
 from django.contrib.auth.models import User
 
@@ -10,7 +9,6 @@ class Product(models.Model):
     image = models.ImageField(upload_to="products/", blank=True, null=True)
     price = models.DecimalField(default=Decimal("0"), decimal_places=5, max_digits=10)
     description = models.TextField(blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
 
     def __str__(self):
         return f"Product : {self.title} - {self.price}"
@@ -19,6 +17,7 @@ class Customer(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=250, null= True)
     email = models.CharField(max_length=200)
+
 
     def __str__(self):
         return self.name
@@ -29,14 +28,17 @@ class Order(models.Model):
     complete = models.BooleanField(default=False)
     transaction_id = models.CharField(max_length=100, null=True)
 
+
     def str(self):
         return str(self.id)
+
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
 
 class Address(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
