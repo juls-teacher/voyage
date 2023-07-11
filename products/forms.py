d75cdea18772301e853c27aa322f37e57d0d1bb4
juls-teacher <juliayurchyk@gmail.com>
2023-07-11 12:14:07 +0300

    add check form email

--- products/forms.py
+++ products/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
 class AddProductForm(forms.Form):
@@ -12,6 +13,12 @@ class RegisterForm(forms.Form):
     name = forms.CharField(max_length=255)
     email = forms.EmailField()
     password = forms.CharField(min_length=8, widget=forms.PasswordInput())
+
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Такая почта уже существует")
+        return email
 
 
 class LoginForm(forms.Form):
